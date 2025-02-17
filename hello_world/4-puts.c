c175432cc8a43b2f7596532bbf1f82966bff2cbe
mia-mika-tech <10580@holbertonstudents.com>
2025-02-17 15:23:29 +0000

    C

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints a specific sentence to the standard output.
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
