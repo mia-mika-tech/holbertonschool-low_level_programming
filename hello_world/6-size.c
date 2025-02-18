8cefe184748c27875fe2236110b15471f8f64d10
mia-mika-tech <10580@holbertonstudents.com>
2025-02-18 08:51:32 +0000

    C

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,12 +5,15 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-  printf("Size of char data type: %u byte(s)\n", sizeof(char));
-  printf("Size of int data type: %u byte(s)\n", sizeof(int));
-  printf("Size of float data type: %u byte(s)\n", sizeof(float));
-  printf("Size of double data type: %u byte(s)\n", sizeof(double));
+  printf("Size of char data type: %lu byte(s)\n", sizeof(char));
+  printf("Size of int data type: %lu byte(s)\n", sizeof(int));
+  printf("Size of a long int data type: %lu byte(s)\n", sizeof(long int));
+  printf("Size of a long long int data type: %lu byte(s)\n", sizeof(long long int));
+  printf("Size of float data type: %lu byte(s)\n", sizeof(float));
+  
 
   return (0);
 }
