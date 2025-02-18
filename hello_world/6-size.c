7988e13decd68c9e2e6e02f14ee063abc70e3b56
mia-mika-tech <10580@holbertonstudents.com>
2025-02-18 08:43:27 +0000

    C

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  printf("Size of char data type: %u byte(s)\n", sizeof(char));
+  printf("Size of int data type: %u byte(s)\n", sizeof(int));
+  printf("Size of float data type: %u byte(s)\n", sizeof(float));
+  printf("Size of double data type: %u byte(s)\n", sizeof(double));
+
+  return (0);
+}
