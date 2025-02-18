d26a7c29a475a2152bf9dc09cd8da8a5fc64ee6b
mia-mika-tech <10580@holbertonstudents.com>
2025-02-18 14:27:23 +0000

    C

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,15 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Description: Generates a random number and determines if it's
+ * positive, negative, or zero
+ *
+ * Return: Always 0 (Success)
+*/
 
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
