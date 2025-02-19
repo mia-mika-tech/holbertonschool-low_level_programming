cb011433965cfb0ceb2400109b7c9ea1d84cbf7d
mia-mika-tech <10580@holbertonstudents.com>
2025-02-19 12:59:23 +0000

    C

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -9,13 +9,13 @@
 
 int main(void)
 {
-        char ch;
+	char ch;
 
-	for(ch = 'a'; ch <= 'z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-	putchar(ch);
+		putchar(ch);
 	}
 	putchar('\n');
-	
+
 	return (0);
 }
