196594d32d5daea070e6785930b38743e89375d9
mia-mika-tech <10580@holbertonstudents.com>
2025-02-19 16:46:43 +0000

    C

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+	char ch;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(n);
+	}
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
