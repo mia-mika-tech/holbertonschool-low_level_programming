8c7bbfc30a2bbea62dbb98796b0e1e0f6b4bead9
mia-mika-tech <10580@holbertonstudents.com>
2025-02-20 15:19:02 +0000

    C

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch;
+
+	for (ch == '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+		putchar(',');
+	}
+	putchar('\n');
+
+	return (0);
+}
