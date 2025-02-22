e8a9c5d48c99690dbe51faad80e56df11765a7e1
mia-mika-tech <10580@holbertonstudents.com>
2025-02-22 10:36:56 +0000

    C

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - Entry point
+ * print_putchar
+ * Return: Always 0 (Success)
+*/
+
+void print_putchar(void)
+{
+       	char ch = "_putchar";
+	int n;
+
+	for (n = 0; ch'n' != '\0'; i++)
+	{
+		_putchar(ch'n');
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
