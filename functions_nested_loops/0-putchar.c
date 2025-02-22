2a8a403104bef46bed67b8dd8761ed2a2387ea3f
mia-mika-tech <10580@holbertonstudents.com>
2025-02-22 13:32:15 +0000

    C

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,10 +1,21 @@
 #include "main.h"
 
-int main(void) {
-    char *ch = "_putchar\n";
-    while (*ch) {
-        _putchar(*ch);
-        ch++;
-    }
-    return 0;
+/**
+ * main - Entry point
+ *
+ * print_putchar - Prints the string "_putchar" followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char *ch = "_putchar\n";
+
+	while (*ch)
+	{
+		_putchar(*ch);
+		ch++;
+	}
+	return (0);
 }
