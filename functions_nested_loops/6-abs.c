b77e692a0e5663e75afd2aa9289d26ba0864c0f4
mia-mika-tech <10580@holbertonstudents.com>
2025-02-23 20:19:44 +0000

    C

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -2,19 +2,20 @@
 
 /**
  * _abs - Calculates the absolute value of an integer
- * @n: The integer | process 
+ * @n: The integer | process
  *
  * Return: The absolute value of n
  */
 
 int _abs(int n)
 {
-    if (n < 0)
-    {
-        return -n;
-    }
-    else
-    {
-        return n;
-    }
+	if (n < 0)
+	{
+		return -n;
+	}
+
+	else
+	{
+		return n;
+	}
 }
