ca625032a7954a7353763745d315f11f61ab9278
mia-mika-tech <10580@holbertonstudents.com>
2025-02-24 13:32:15 +0000

    C

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -3,7 +3,8 @@
 /**
  * jack_bauer - Shows every minute of Jack Bauer's day
  *
- * Description: This function scrolls through all hours (00 | 23) and minutes (00 | 59) and displays                  them in HH:MM format.
+ * Description: This function scrolls through all hours (00 | 23) and minutes (00 | 59) 
+ *              and displays them in HH:MM format.
  * minutes (00 | 59) and displays them in HH:MM format.
  */
 
@@ -13,15 +14,18 @@ void jack_bauer(void)
 
     for (hour = 0; hour < 24; hour++)
     {
-        for (minute = 0; minute < 60; minute++) 
-        {
-            _putchar((hour / 10) + '0');
-            _putchar((hour % 10) + '0');
-            _putchar(':');
+	    
+	    for (minute = 0; minute < 60; minute++) 
+	    {
+		    _putchar((hour / 10) + '0');
+		    _putchar((hour % 10) + '0');
+		    
+		    _putchar(':');
 
-            _putchar((minute / 10) + '0');
-            _putchar((minute % 10) + '0');
-            _putchar('\n');
-        }
+		    _putchar((minute / 10) + '0');
+		    _putchar((minute % 10) + '0');
+		    
+		    _putchar('\n');
+	    }
     }
 }
