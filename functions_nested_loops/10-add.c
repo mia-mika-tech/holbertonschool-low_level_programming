e102e63bcc2e52eb8428f3b77725890c6bc33234
mia-mika-tech <10580@holbertonstudents.com>
2025-02-24 17:06:28 +0000

    C

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -2,13 +2,11 @@
 
 /**
  * add - Adds two integers and returns the result.
- * @a: The first integer.
- * @b: The second integer.
  *
  * Return: The sum of a and b.
  */
 
 int add(int a, int b)
 {
-    return (a + b);
+	return (a + b);
 }
