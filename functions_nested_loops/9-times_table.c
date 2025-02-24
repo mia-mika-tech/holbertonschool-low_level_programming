440f315e22e1fdd97fbde3692082b62a1b7b3260
mia-mika-tech <10580@holbertonstudents.com>
2025-02-24 15:34:37 +0000

    C

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -6,34 +6,36 @@
 
 void times_table(void)
 {
+	int i, j, result;
 
-    	int i, j, result;
+	for (i = 0; i <= 9; i++)
+	{
 
-    for (i = '0'; i <= '9'; i++)
-    {
+		for (j = 0; j <= 9; j++)
+		{
 
-        for (j = '0'; j <= '9'; j++)
-        {
-            result = 'i' * 'j';
+			result = i * j;
 
-            if (j == 0)
-            {
-                _putchar('0' + result);
-            }
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
+			if (j == 0)
+		{
+			_putchar('0' + result);
+		}
 
-                if (result < 10)
-                {
-                    _putchar(' ');
-                }
-                _putchar('0' + (result / 10));
-                _putchar('0' + (result % 10));
-            }
-        }
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
 
-        _putchar('\n');
-    }
+				if (result < 10)
+				{
+					_putchar(' ');
+				}
+
+				_putchar('0' + (result / 10));
+				_putchar('0' + (result % 10));
+			}
+		}
+
+		_putchar('\n');
+	}
 }
