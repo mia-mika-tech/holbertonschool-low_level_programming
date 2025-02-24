e664d0740acf709a8298c770aae85247cf52d039
mia-mika-tech <10580@holbertonstudents.com>
2025-02-24 17:23:57 +0000

    C

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -11,29 +11,32 @@
 
 void print_to_98(int n)
 {
-    if (n <= 98)
-    {
-        
-        for (; n <= 98; n++)
+	if (n <= 98)
+	{
+
+		for (; n <= 98; n++)
         {
-            printf("%d", n);
-            if (n != 98)
-            {
-                printf(",");
-            }
-        }
-    }
-    else
-    {
-       
+		printf("%d", n);
+
+		if (n != 98)
+		{
+			printf(", ");
+		}
+	}
+	}
+
+	else
+	{
+
         for (; n >= 98; n--)
         {
-            printf("%d", n);
-            if (n != 98)
-            {
-                printf(",");
-            }
-        }
-    }
-    printf("\n");
+		printf("%d", n);
+
+		if (n != 98)
+		{
+			printf(", ");
+		}
+	}
+	}
+	printf("\n");
 }
