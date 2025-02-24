d096c7aa5de023ef06656d09494163e704d6808b
mia-mika-tech <10580@holbertonstudents.com>
2025-02-24 14:40:46 +0000

    C

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -12,22 +12,23 @@
 
 void jack_bauer(void)
 {
-    int hour, minute;
+	int hour, minute;
 
-    for (hour = 0; hour < 24; hour++)
-    {
-       
-        for (minute = 0; minute < 60; minute++)
-        {
-            _putchar((hour / 10) + '0');
-            _putchar((hour % 10) + '0');
+	for (hour = 0; hour < 24; hour++)
+	{
 
-            _putchar(':');
+		for (minute = 0; minute < 60; minute++)
+		{
 
-            _putchar((minute / 10) + '0');
-            _putchar((minute % 10) + '0');
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
 
-            _putchar('\n');
-        }
-    }
+			_putchar(':');
+
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+
+			_putchar('\n');
+		}
+	}
 }
