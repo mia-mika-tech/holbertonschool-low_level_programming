95f55fb10e51e53ea2d77e0b27955886b4b65d39
mia-mika-tech <10580@holbertonstudents.com>
2025-02-24 14:48:24 +0000

    C

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * times_table - Imprime la table des 9 fois, en commençant par 0.
+ */
+
+void times_table(void)
+{
+
+    	int i, j, result;
+
+    for (i = 0; i <= 9; i++)
+    {
+
+        for (j = 0; j <= 9; j++)
+        {
+            result = i * j;
+
+            if (j == 0)
+            {
+                _putchar('0' + result);
+            }
+            else
+            {
+                _putchar(',');
+                _putchar(' ');
+
+                if (result < 10)
+                {
+                    _putchar(' ');
+                }
+                _putchar('0' + (result / 10));
+                _putchar('0' + (result % 10));
+            }
+        }
+
+        _putchar('\n');
+    }
+}
